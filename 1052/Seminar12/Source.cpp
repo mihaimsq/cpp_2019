d2d396f96da357db23ba0c5cc5e665fd4e4e19b7
Catalin <catalin.boja@hotmail.com>
2019-12-16 12:04:50 +0200

    Week 12 partial update

--- 1052/Seminar12/Source.cpp
+++ 1052/Seminar12/Source.cpp
@@ -22,12 +22,15 @@ public:
 		cout << endl << "Destructor student";
 	}
 
-	void info() {
+	virtual void info() {
 		cout << endl << "-----------------------";
 		cout << endl << "Nume: " << this->nume;
 		cout << endl << "Varsta: " << this->varsta;
 		cout << endl << "Facultate: " << this->facultate;
 	}
+
+	//functie virtuala pura - nu are corp
+	virtual void functieObligatorie() = 0;
 };
 
 class Masterand : public Student {
@@ -48,6 +51,33 @@ public:
 	~Masterand() {
 		cout << endl << "Apel destructor masterand";
 	}
+
+	void printMasterand() {
+		this->info();
+		cout << endl << "Program masterat " << this->programMasterat;
+	}
+
+	//supraincarcare
+	void ceva(int vb) {
+
+	}
+	void ceva(int a, int b) {
+
+	}
+	void ceva(string a, float b) {
+
+	}
+
+	//supradefinire
+	void info() {
+		this->Student::info();
+		cout << endl << "Program masterat " << this->programMasterat;
+	}
+
+	void functieObligatorie() {
+
+	}
+
 };
 
 class Intern: public Student {
@@ -63,14 +93,44 @@ public:
 	~Intern() {
 		cout << endl << "Apel destructor Intern";
 	}
+
+	//supradefinire
+	void info() {
+		this->Student::info();
+		cout << endl << "Companie practica " << this->companie;
+	}
 };
 
 int main() {
+
+	//nu mai merge cu clasa abstracta
 	Student s1;
-	Student s2("Gigel", 21, "CSIE");
+	Student s2("Student Gigel", 21, "CSIE");
 
-	s1.info();
-	s2.info();
+	//s1.info();
+	//s2.info();
 
-	Masterand m1;
+	Masterand m1("Masterand Ionel", 22, "CSIE","IE");
+	Intern i1("Intern Ana", 21, "CSIE", "Intel");
+
+	//m1.info();
+
+	//Student lista[4];
+	//lista[0] = s1;
+	//lista[1] = s2;
+	//lista[2] = m1;		//up-casting
+	//lista[3] = i1;
+
+	Student* lista2[4];
+
+	lista2[0] = &s1;
+	lista2[1] = &s2;
+	lista2[2] = &m1;	//up casting la nivel de pointer
+	lista2[3] = &i1;	//up casting la nivel de pointer
+
+	for (int i = 0; i < 4; i++) {
+		lista2[i]->info();	//early-binding fara virtual
+		//late-binding cu virtual
+	}
+
 }
